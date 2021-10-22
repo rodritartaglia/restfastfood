57dc553febba2bd5532d3a49bacacd8b0cdb6ba2
rodritartaglia <90225168+rodritartaglia@users.noreply.github.com>
2021-10-22 00:51:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 
 const menuButton = document.getElementById("icon-navbar");
-const navbar = document.getElementById("navbarMenu");
+const navbarMenu = document.getElementById("navbarMenu");
+const navbar = document.getElementsByClassName("home-navbar")[0];
 
 function toggleMenu() {
-    navbar.classList.toggle("active");
+    navbarMenu.classList.toggle("active");
 }
 
-menuButton.addEventListener("click",toggleMenu);
+
+
+menuButton.addEventListener("click", function () {
+    toggleMenu();
+    if (navbar.style.backgroundColor == "#0e0e0e") {
+        navbar.style.backgroundColor = "transparent";
+        navbar.style.position = "";
+    }
+    else {
+        navbar.style.backgroundColor = "#0e0e0e";
+        navbar.style.position = "fixed";
+    };
+}
+);
 
 
