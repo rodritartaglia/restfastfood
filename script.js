a5a783f1892dc95cc0adf6354667a9457c84ca6e
rodritartaglia <90225168+rodritartaglia@users.noreply.github.com>
2021-10-23 23:59:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const navButton = document.getElementById("icon-navbar");
 const navbarMenu = document.getElementById("navbarMenu");
 const navbar = document.getElementsByClassName("home-navbar")[0];
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
 
 let home = document.getElementById("homeNav");
 let about = document.getElementById("aboutNav");
@@ -18,6 +20,4 @@ home.addEventListener("click", toggleMenu);
 about.addEventListener("click", toggleMenu);
 menu.addEventListener("click", toggleMenu);
 contact.addEventListener("click", toggleMenu);
-
-
 
